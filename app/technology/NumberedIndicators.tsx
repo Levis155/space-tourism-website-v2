32e7023e0a54950a492414cf0c6c0318a00ee0bf
Levis155 <levismbui@gmail.com>
2025-11-07 16:55:34 +0300

    Clean up the NumberedIndicators component

--- app/technology/NumberedIndicators.tsx
+++ app/technology/NumberedIndicators.tsx
@@ -34,33 +34,6 @@ const NumberedIndicators = ({ name }: { name: string }) => {
           {technology.label}
         </button>
       ))}
-      {/* <button
-        aria-selected="true"
-        role="tab"
-        aria-controls="vehicle-tab"
-        data-image="vehicle-image"
-        className="bg-dark text-white fs-600 ff-serif"
-      >
-        1
-      </button>
-      <button
-        aria-selected="false"
-        role="tab"
-        aria-controls="spaceport-tab"
-        data-image="spaceport-image"
-        className="bg-dark text-white fs-600 ff-serif"
-      >
-        2
-      </button>
-      <button
-        aria-selected="false"
-        role="tab"
-        aria-controls="capsule-tab"
-        data-image="capsule-image"
-        className="bg-dark text-white fs-600 ff-serif"
-      >
-        3
-      </button> */}
     </div>
   );
 };
